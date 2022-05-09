6a4799c562b10c82d035964d0eaf0e8abfd8af11
Nimo Abdi <nimoabdi318@gmail.com>
2022-05-09 11:38:11 -0500

    java

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log('Im in script.js')
+
+let myName = 'Nimo';
+console.log('my name is Nimo');
+// console.log('my name is ',' 'Nimo');
+
+let myLastName = 'Abdi';
+console.log('my last name is', myLastName);
+/**
+ * Expresssions
+ * 
+*/
+
+
