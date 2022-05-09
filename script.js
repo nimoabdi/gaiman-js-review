7aced84124bbc63c15cdf8865ecb6e94262fef7b
Nimo Abdi <nimoabdi318@gmail.com>
2022-05-09 13:23:00 -0500

    conditionals ewwww

--- script.js
+++ script.js
@@ -7,8 +7,31 @@ console.log('my name is Nimo');
 let myLastName = 'Abdi';
 console.log('my last name is', myLastName);
 /**
- * Expresssions
- * 
+* Expresssions
+* 
 */
+function add (n1, n2) {
+    return n1 + n2;
+}
+let result = add(6, 10);
+//let result = 16;
+let isSomethingTrue = 'Nimo'
 
 
+if (8>7) {
+    // if (false) {.....}
+}
+// NaN = not a number
+
+
+//conditionals 
+
+let age = '23'
+if (age >56 ) {
+    console.log('you are young');
+}
+else if (age <56) {
+    console.log('you are old');
+}
+
+// functions
